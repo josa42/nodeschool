668d5c85f4dbed437f93590037660cf64736a1f3
Josa Gesell <josa@gesell.me>
2014-12-29 00:36:32 +0100

    Solve: Functional Javascript - 008 - Basic: Call

---
+++ functional-javascript/008_basic_call.js
@@ -0,0 +1,11 @@
+// Basic: Call
+
+function duckCount() {
+  return Array.prototype
+    .filter.call(arguments, function(obj) {
+      return Object.prototype.hasOwnProperty.call(obj, 'quack');
+    })
+    .length;
+}
+
+module.exports = duckCount;
